c785f98d242ddfd6e2a76e9b7863c38acc57e0c8
jobm <jobmwaniki18@gmail.com>
2016-03-08 00:01:57 +0300

    added hover animated effects to work

--- js/workjs.js
+++ js/workjs.js
@@ -1,11 +1,8 @@
 $(function(){
-  $(".fa-link").hide();
   $(".card").hover(function() {
-      $(this).fadeTo("slow", 0.85).css('z-index',-100000);
-      $(this).find(".fa-link").show().css('z-index',10000);
+      $(this).find(".image-overlay").show("slow");
  },
  function() {
-    $(this).stop().fadeTo("slow", 1);
-    $(this).find(".fa-link").hide();
+      $(this).find(".image-overlay").hide("slow");
  });
 });
