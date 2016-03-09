bbeb5fb60c0ee7e7462a6e24fb60145508a6c9f6
jobm <jobmwaniki18@gmail.com>
2016-03-09 21:57:14 +0300

    iteration one of work section is done

--- js/workjs.js
+++ js/workjs.js
@@ -1,8 +1,15 @@
 $(function(){
-  $(".card").hover(function() {
-      $(this).find(".image-overlay").show("slow");
- },
- function() {
-      $(this).find(".image-overlay").hide("slow");
- });
+  $(".card").hover(
+    function() {
+      $(this).find(".image-overlay").show();
+      $(this).find(".image-overlay").animate({
+        height: "100%",
+        top: "0.0em"},300);
+      },
+      function() {
+        $(this).find(".image-overlay").animate({
+          display: "none",
+          height: "0px",},300).hide();
+          $(this).find(".image-overlay").hide();
+        });
 });
