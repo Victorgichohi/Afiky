2f3e60aee5c341afeb7f59ccf7dbc02467368b63
jobm <jobmwaniki18@gmail.com>
2016-03-11 04:41:35 +0300

    side bar working, minor bugs to fix

--- js/workjs.js
+++ js/workjs.js
@@ -1,15 +1,28 @@
-$(function(){
+$(function() {
   $(".card").hover(
     function() {
       $(this).find(".image-overlay").show();
       $(this).find(".image-overlay").animate({
         height: "100%",
-        top: "0.0em"},300);
-      },
-      function() {
-        $(this).find(".image-overlay").animate({
-          display: "none",
-          height: "0px",},300).hide();
-          $(this).find(".image-overlay").hide();
-        });
+        top: "0.0em"
+      }, 300);
+    },
+    function() {
+      $(this).find(".image-overlay").animate({
+        display: "none",
+        height: "0px",
+      }, 300).hide();
+      $(this).find(".image-overlay").hide();
+    });
+  $(".menu #menu-open").click(function() {
+    $(".menu-right").toggle().animate({
+      width: "350px"
+    }, 500);
+  });
+  $(".menu-right #menu-close").click(function() {
+    $(".menu-right").animate({
+      width: "0",
+      display: "none"
+    }, 500);
+  });
 });
